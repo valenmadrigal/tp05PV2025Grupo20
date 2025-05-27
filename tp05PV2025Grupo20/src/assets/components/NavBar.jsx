3ed5d7f4aec1910c2ecf52cc81ea270eedcf2a71
TaliaEspejo <taliamariamela18@gmail.com>
2025-05-27 00:55:48 -0300

    modificaciones del NavBar por Espejo Talia

--- tp05PV2025Grupo20/src/assets/components/NavBar.jsx
+++ tp05PV2025Grupo20/src/assets/components/NavBar.jsx
@@ -1,27 +1,22 @@
 import { Link } from 'react-router-dom';
-import '../../css/navbar.css'; // Asegúrate de que la ruta sea correcta
+import '../../css/NavBar.css';
+import logo from '../../css/logo.png'; // Importa el logo
+
 export default function NavBar() {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
         <Link to="/" className="navbar-logo">
-          Gestión Escolar
+          <img src={logo} alt="Logo" className="logo-image" />
+          <span>Gestión Escolar</span>
         </Link>
       </div>
       
       <ul className="navbar-links">
-        <li>
-          <Link to="/" className="nav-link">Inicio</Link>
-        </li>
-        <li>
-          <Link to="/alumnos" className="nav-link">Alumnos</Link>
-        </li>
-        <li>
-          <Link to="/nuevo-alumno" className="nav-link">Nuevo Alumno</Link>
-        </li>
-        <li>
-          <Link to="/acerca" className="nav-link">Acerca de</Link>
-        </li>
+        <li><Link to="/" className="nav-link">Inicio</Link></li>
+        <li><Link to="/alumnos" className="nav-link">Alumnos</Link></li>
+        <li><Link to="/nuevo-alumno" className="nav-link">Nuevo Alumno</Link></li>
+        <li><Link to="/acerca" className="nav-link">Acerca de</Link></li>
       </ul>
     </nav>
   );
